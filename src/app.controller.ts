91349bc06f35d98edc848a5f44aa7e40b1c07ea8
tbriand <briandthibaud99@gmail.com>
2023-03-03 16:25:00 +0100

    Init_project: npm run lint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,7 @@ import { ApiResponse, ApiTags } from '@nestjs/swagger';
 @Controller()
 @ApiTags('HelloWorld')
 export class AppController {
-  constructor(private readonly appService: AppService) {
-  }
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   @HttpCode(200)
