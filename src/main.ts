93e603ed9e6674d8afe15eff181e9ed8529dc6a4
tbriand <briandthibaud99@gmail.com>
2023-03-03 14:27:19 +0100

    Init_project: Refacto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from "@nestjs/core";
-import { AppModule } from "./app.module";
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['*', 'http://127.0.0.1:4200', 'http://localhost', 'http://localhost:4200'],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
+    credentials: true,
+    allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization,authorization,X-Forwarded-for,Set-Cookie,Access-Control-Allow-Origin',
+  });
   await app.listen(3000);
 }
-bootstrap().then(r => console.log("Server started"));
+
+bootstrap().then(r => console.log('Server started'));
