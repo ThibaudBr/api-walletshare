b21bfc81ca71ec71f5bf339bd4cc18d43ec6afac
tbriand <briandthibaud99@gmail.com>
2023-03-05 13:11:59 +0100

    Compodoc: Add compodoc library

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,22 @@ import { ApiResponse, ApiTags } from '@nestjs/swagger';
 @Controller()
 @ApiTags('HelloWorld')
 export class AppController {
+  /**
+   * @ignore
+   */
   constructor(private readonly appService: AppService) {}
 
+  /**
+   * @api {get} / Hello World
+   * @apiName GetHello
+   * @apiGroup HelloWorld
+   * @apiVersion 0.0.1
+   * @apiDescription Hello World!
+   * @apiSuccess {String} text/html Hello World!
+   * @apiSuccessExample {text/html} Success-Response:
+   *    HTTP/1.1 200 OK
+   *    Hello World!
+   */
   @Get()
   @HttpCode(200)
   @ApiResponse({
