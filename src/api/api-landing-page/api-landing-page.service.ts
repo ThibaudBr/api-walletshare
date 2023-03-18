1410ea989991a3ca7460eba6ea53dd8ed2c0613a
tbriand <briandthibaud99@gmail.com>
2023-03-18 14:15:13 +0100

    fix

--- src/api/api-landing-page/api-landing-page.service.ts
+++ src/api/api-landing-page/api-landing-page.service.ts
@@ -3,6 +3,7 @@ import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { DeleteMailCommand } from './cqrs/command/delete-mail.command';
 import { MailResponse } from "./domain/response/mail.response";
 import { GetAllMailQuery } from "./cqrs/query/get-all-mail.query";
+import { MailLandingPageDto } from "./domain/dto/mail-landing-page.dto";
 
 @Injectable()
 export class ApiLandingPageService {
@@ -17,6 +18,11 @@ export class ApiLandingPageService {
   }
 
   async getAll(): Promise<MailResponse[]> {
-    const mailResponseList: MailResponse[] = await this.queryBus.execute(new GetAllMailQuery());
+    const mailResponseList: MailLandingPageDto[] = await this.queryBus.execute(new GetAllMailQuery());
+    return mailResponseList.map((mailLandingPageDto: MailLandingPageDto) => {
+      return {
+        mail: mailLandingPageDto.mail,
+      };
+    });
   }
 }
