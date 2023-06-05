d6be571ff4018688c49a05ac2b3f5e5d018bb6c2
tBriand <briandthibaud99@gmail.com>
2023-06-05 18:13:20 +0200

    test 2

--- src/api/api-log/application/cqrs/handler/command/create-log.command-handler.ts
+++ src/api/api-log/application/cqrs/handler/command/create-log.command-handler.ts
@@ -27,7 +27,7 @@ export class CreateLogCommandHandler implements ICommandHandler<CreateLogCommand
     command.apiName = this.API_NAME;
     command.apiVersion = this.npm_package_version;
     command.apiType = this.API_TYPE;
-    console.log(this.client.emit('create-log', command));
+    console.log(await this.client.connect());
     this.client.emit('create-log', command);
   }
 }
