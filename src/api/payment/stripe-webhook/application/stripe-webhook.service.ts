6fbf2e1e4738f04e70b745c64bf9bca4e713b47f
tBriand <briandthibaud99@gmail.com>
2023-06-12 14:42:48 +0200

    add console.log()

--- src/api/payment/stripe-webhook/application/stripe-webhook.service.ts
+++ src/api/payment/stripe-webhook/application/stripe-webhook.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CommandBus, EventBus } from '@nestjs/cqrs';
 import { SubscriptionService } from '../../subscription/application/subscription.service';
 import { CreateStripeEventCommand } from './cqrs/command/create-stripe-event.command';
@@ -10,7 +10,6 @@ import { ProductService } from '../../product/application/product.service';
 import { ProductResponse } from '../../product/web/response/product.response';
 import { GetUserByStripeCustomerIdQuery } from './cqrs/query/get-user-by-stripe-customer-id.query';
 import { ProfileService } from '../../../profile/application/profile.service';
-import { RoleProfileEnum } from '../../../profile/domain/enum/role-profile.enum';
 
 @Injectable()
 export class StripeWebhookService {
@@ -23,25 +22,35 @@ export class StripeWebhookService {
     private readonly profileService: ProfileService,
   ) {}
 
-  async processSubscriptionUpdate(event: Stripe.Event, subscription: Stripe.Subscription): Promise<void> {
-    await this.commandBus
-      .execute(
-        new CreateStripeEventCommand({
-          stripEventId: event.id,
-        }),
-      )
-      .catch(async (error: Error) => {
-        if (error.message === 'StripEvent already exist') {
-          return;
-        }
-      });
+  async processSubscriptionUpdate(event: Stripe.Event): Promise<void> {
+    console.log(event);
+    console.log(event.type);
+    console.log(event.data.object);
+    switch (event.type) {
+      case 'customer.subscription.update':
+        await this.commandBus
+          .execute(
+            new CreateStripeEventCommand({
+              stripEventId: event.id,
+            }),
+          )
+          .catch(async (error: Error) => {
+            if (error.message === 'StripEvent already exist') {
+              return;
+            }
+          });
 
-    const data: Stripe.Subscription = event.data.object as Stripe.Subscription;
+        const data: Stripe.Subscription = event.data.object as Stripe.Subscription;
 
-    const customerId: string = data.customer as string;
-    const subscriptionStatus: Stripe.Subscription.Status = data.status;
+        const customerId: string = data.customer as string;
+        const subscriptionStatus: Stripe.Subscription.Status = data.status;
 
-    await this.subscriptionService.updateMonthlySubscriptionStatus(customerId, subscriptionStatus);
+        await this.subscriptionService.updateMonthlySubscriptionStatus(customerId, subscriptionStatus);
+        break;
+      default:
+        console.log(event);
+        break;
+    }
   }
 
   async processPayment(event: Stripe.Event, jsonObject: object): Promise<void> {
