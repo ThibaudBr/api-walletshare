b376d6c5a5659db73163fd52a8be237afa2dfe72
tBriand <briandthibaud99@gmail.com>
2023-06-19 10:45:39 +0200

    update cors, remove swagger in remote

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap(): Promise<void> {
     .setVersion(process.env.npm_package_version ?? '0.0.1')
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, );
 
   const configService: ConfigService = app.get(ConfigService);
   // Set the config options
@@ -35,14 +35,21 @@ async function bootstrap(): Promise<void> {
   admin.initializeApp(firebaseConfig);
 
   app.enableCors({
-    origin: [
-      '*',
-      configService.get<string>('HOST_DASHBOARD_ADMIN') ?? 'http://localhost:8080',
-      configService.get<string>('HOST_DASHBOARD_CLIENT') ?? 'http://localhost:8081',
-      configService.get<string>('HOST_API_WAITING_LIST') ?? 'http://localhost:3003',
-      configService.get<string>('HOST_API_MAIL') ?? 'http://localhost:3002',
-      configService.get<string>('HOST_API_LOG') ?? 'http://localhost:3001',
-    ],
+    origin: function (origin, callback) {
+      const whitelist = [
+        configService.get<string>('HOST_DASHBOARD_ADMIN') ?? 'http://localhost:8080',
+        configService.get<string>('HOST_DASHBOARD_CLIENT') ?? 'http://localhost:8081',
+        configService.get<string>('HOST_API_WAITING_LIST') ?? 'http://localhost:3003',
+        configService.get<string>('HOST_API_MAIL') ?? 'http://localhost:3002',
+        configService.get<string>('HOST_API_LOG') ?? 'http://localhost:3001',
+      ];
+
+      if (whitelist.indexOf(origin) !== -1 || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     preflightContinue: false,
     optionsSuccessStatus: 200,
@@ -50,7 +57,6 @@ async function bootstrap(): Promise<void> {
     allowedHeaders:
       'Origin,X-Requested-With,Content-Type,Accept,Authorization,authorization,X-Forwarded-for,Set-Cookie,Access-Control-Allow-Origin',
   });
-
   await app.listen(configService.get<string>('PORT') ?? 3000);
   console.log('[LOG] Server started on port ' + configService.get<string>('PORT'));
 }
