225211c0bb4a0d6ebc40075cb8a0293a4354efca
tBriand <briandthibaud99@gmail.com>
2023-06-19 13:28:00 +0200

    add subscription-invoices.subscriber

---
+++ src/api/payment/invoices/application/subscriber/subscription-invoice.subscriber.ts
@@ -0,0 +1,46 @@
+import { SubscriptionEntity } from '../../../subscription/domain/entities/subscription.entity';
+import { EntitySubscriberInterface, EventSubscriber, RemoveEvent, Repository, SoftRemoveEvent } from 'typeorm';
+import { InvoicesEntity } from '../../domain/entities/invoices.entity';
+
+@EventSubscriber()
+export class SubscriptionInvoiceSubscriber implements EntitySubscriberInterface<SubscriptionEntity> {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types,@typescript-eslint/explicit-function-return-type
+  listenTo() {
+    return SubscriptionEntity;
+  }
+
+  async beforeRemove(event: RemoveEvent<SubscriptionEntity>): Promise<void> {
+    const removedSubscription: SubscriptionEntity | undefined = event.entity;
+    const invoiceRepository: Repository<InvoicesEntity> = event.manager.getRepository(InvoicesEntity);
+    const invoices: InvoicesEntity[] = await invoiceRepository.find({
+      relations: ['subscription'],
+      withDeleted: true,
+      where: {
+        subscription: {
+          id: removedSubscription?.id,
+        },
+      },
+    });
+    if (invoices.length == 0) return;
+    for (const invoice of invoices) {
+      await invoiceRepository.remove(invoice);
+    }
+  }
+
+  async beforeSoftRemove(event: SoftRemoveEvent<SubscriptionEntity>): Promise<void> {
+    const softRemovedSubscription: SubscriptionEntity | undefined = event.entity;
+    const invoiceRepository: Repository<InvoicesEntity> = event.manager.getRepository(InvoicesEntity);
+    const invoices: InvoicesEntity[] = await invoiceRepository.find({
+      relations: ['subscription'],
+      where: {
+        subscription: {
+          id: softRemovedSubscription?.id,
+        },
+      },
+    });
+    if (invoices.length == 0) return;
+    for (const invoice of invoices) {
+      await invoiceRepository.softRemove(invoice);
+    }
+  }
+}
