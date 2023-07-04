53ddc1a64c7c2cc3bb7b8ba450dd91835c300770
tBriand <briandthibaud99@gmail.com>
2023-07-04 04:09:28 +0200

    remove console.log in middleware

--- src/middleware/json-body.middleware.ts
+++ src/middleware/json-body.middleware.ts
@@ -5,7 +5,6 @@ import * as bodyParser from 'body-parser';
 @Injectable()
 export class JsonBodyMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction): void {
-    console.log('JsonBodyMiddleware');
     bodyParser.json()(req, res, next);
   }
 }
--- src/middleware/raw-body.middelware.ts
+++ src/middleware/raw-body.middelware.ts
@@ -5,7 +5,6 @@ import * as bodyParser from 'body-parser';
 @Injectable()
 export class RawBodyMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction): void {
-    console.log('RawBodyMiddleware');
     bodyParser.raw({ type: '*/*' })(req, res, next);
   }
 }
