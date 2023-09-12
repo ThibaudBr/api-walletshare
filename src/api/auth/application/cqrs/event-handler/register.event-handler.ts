002877bc39ec8c5954b7022a31e5790a3dabd5db
ThibaudB <calimero7899@gmail.com>
2023-09-12 10:08:15 +0200

    Fix log on register

--- src/api/auth/application/cqrs/event-handler/register.event-handler.ts
+++ src/api/auth/application/cqrs/event-handler/register.event-handler.ts
@@ -2,14 +2,16 @@ import { EventsHandler, IEventHandler } from '@nestjs/cqrs';
 import { Logger } from '@nestjs/common';
 import { logger } from '../../../../../util/config/winston-logger.config';
 import { RegisterEvent } from '../event/register.event';
+import { ApiLogService } from '../../../../api-log/application/api-log.service';
 
 @EventsHandler(RegisterEvent)
 export class RegisterEventHandler implements IEventHandler<RegisterEvent> {
-  logger_console = new Logger('RegisterEventHandler');
-
-  handle(event: RegisterEvent): void {
-    logger.info('New user have registered  with id ' + event.userId);
-
-    this.logger_console.log('New user have registered with id ' + event.userId);
+  constructor(private readonly apiLogService: ApiLogService) {}
+  async handle(event: RegisterEvent): Promise<void> {
+    await this.apiLogService.createLogForMethode({
+      method: event.method,
+      module: event.module,
+      body: `New user with ${event.userId} have registered`,
+    });
   }
 }
--- src/api/auth/application/cqrs/event/register.event.ts
+++ src/api/auth/application/cqrs/event/register.event.ts
@@ -1,3 +1,3 @@
 export class RegisterEvent {
-  constructor(public readonly userId: string) {}
+  constructor(public readonly userId: string, public readonly method: 'Register', public readonly module: 'Auth') {}
 }
